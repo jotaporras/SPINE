6337f840f30c99e5d1796d9be747f1d54e6f60c6
zerozerodone <arushar@seas.upenn.edu>
2026-03-18 22:33:31 -0400

    Added support for parametrized removal of ICL examples and system prompts based on discussions from Javier, Zac, and Arush Sync held on March 16.

--- src/spine/spine.py
+++ src/spine/spine.py
@@ -43,6 +43,7 @@ class SPINE:
         model=None,
         tokenizer=None,
         client=None,
+        use_icl: bool = True,
     ) -> None:
         self.graph = graph
         if client is not None:
@@ -56,6 +57,7 @@ class SPINE:
         else:
             raise ValueError(f"llm type: {llm} unsupported")
 
+        self.use_icl = use_icl
         self.n_attempts = 3
         self.base_request = ""
 
@@ -382,7 +384,7 @@ class SPINE:
 
         msg = (
             get_base_prompt_update_graph(
-                request=self.base_request, scene_graph=self.graph.as_json_str
+                request=self.base_request, scene_graph=self.graph.as_json_str, use_icl=self.use_icl
             )
             + self.msg_history
         )
